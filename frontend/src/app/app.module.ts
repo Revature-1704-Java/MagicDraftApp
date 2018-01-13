c21f831bcff0281e540bb8c809fbb19bb7441e83
unknown <jack.mccabev@gmail.com>
2018-01-12 22:06:24 -0500

    Created the frontend components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { DraftInterfaceComponent } from './draft-interface/draft-interface.component';
+import { DraftSummaryComponent } from './draft-summary/draft-summary.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomePageComponent,
+    FooterComponent,
+    DraftInterfaceComponent,
+    DraftSummaryComponent
   ],
   imports: [
     BrowserModule
