7039bba464b52835034c07c08b577d81d29fda51
unknown <jack.mccabev@gmail.com>
2018-01-15 12:42:54 -0500

    Reverted to test login function

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../shared/user'
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +7,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {  }
-
-  ngAfterViewInit() {
-      document.getElementById('login').addEventListener('click', this.validateLogin);
-  }
-
-  validateLogin() {
-    let self = this.http;
-    let user : string = (<HTMLInputElement>document.getElementById('username')).value;
-    let pass : string = (<HTMLInputElement>document.getElementById('password')).value;
-    let loginInfo : User = {email:user, password:pass};
-    console.log(this);
-    console.log(this.http);
-    self.post('http://18.216.223.139:8090/login', loginInfo).subscribe(
-      res => {
-        console.log(res)
-      }
-    )
-
-  }
 
 }
