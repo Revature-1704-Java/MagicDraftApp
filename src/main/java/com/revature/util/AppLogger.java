98fea5655687020095cbad6584531b91cc06e70e
Jonathan <jonchengc3@gmail.com>
2018-01-17 14:02:11 -0500

    up-to-date and Log4j
    merged changes ONTO this branch and added some log4j methods

--- src/main/java/com/revature/util/AppLogger.java
+++ src/main/java/com/revature/util/AppLogger.java
@@ -20,4 +20,22 @@ public class AppLogger {
 		logger.info(args[0] + " attemping to login.");
 	}
 	
+	// additional log4j tests 1/16/18, 9:35 PM EDT
+	@AfterReturning("execution(* com.revature.controller.UserValidation.postLogout())")
+	public void logginOut(JoinPoint jp) {
+		Object[] args = jp.getArgs();
+	}
+	
+	@AfterReturning("execution(* com.revature.controller.UserValidation.postSignup(..))")
+	public void logginPostSignup(JoinPoint jp) {
+		Object[] args = jp.getArgs();
+		logger.info(args[0] + " attemping to process info after logging in.");
+	}
+	
+	@AfterReturning("execution(* com.revature.controller.DeckController.saveDeck(..))")
+	public void logginSaveDeck (JoinPoint jp) {
+		Object[] args = jp.getArgs();
+		logger.info(args[0] + " attempting to save dat deck. ._.");
+	}
+	
 }
