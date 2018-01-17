dd667d5a83c01a4e68f3e0a32eac9e2dfcd235c2
Dcosiem <david.cosiem@yahoo.com>
2018-01-17 12:12:32 -0500

    Working on Displaying Images

--- frontend/src/app/draft-interface/draft-interface.component.ts
+++ frontend/src/app/draft-interface/draft-interface.component.ts
@@ -12,41 +12,50 @@ export class DraftInterfaceComponent implements OnInit {
 
   ngOnInit() {
     let xhttp = new XMLHttpRequest();
+    let pack_flag = 1;
     xhttp.open("POST","http://18.216.223.139:8090/generate/pack/players/2", true);
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
         let json = xhttp.responseText;
         // document.getElementById("demo").innerHTML = json;
         let obj = JSON.parse(json);
-        for(var i =0; i < obj.length; i++){
-          var temp = obj[i];
-          for(var j =0; j < obj[i].length; j++){
-            console.log(temp[j].name);
-            let imgurl = "<img src=\"" + grabImage(temp[j].name) + "\">";
-            console.log(imgurl);
-            document.getElementById("demo").innerHTML += ("<img src=\"" + grabImage(temp[j].name) + ">");
-          }
+        // sessionStorage.setItem()
+        let pack = obj[0];
+        for(let i = 0; i < pack.length; i++){
+          console.log(pack[i].name);
+          grabImage(pack[i].name);
         }
       }
     };
     xhttp.send();
   }
-  function grabImage(cardName:string){
-    let xhttp = new XMLHttpRequest();
-    let url = "https://api.magicthegathering.io/v1/cards?name=" + cardName;
-    let answer = "";
+}
+function grabImage(cardName:string){
+  let xhttp = new XMLHttpRequest();
+  let url = "https://api.magicthegathering.io/v1/cards?name=" + cardName;
+  let answer = "";
 
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        let json = this.responseText;
-        let obj = JSON.parse(json);
-        let image = obj.cards;
-        answer = image[0].imageUrl;
-        console.log(answer);
-        return answer;
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let json = this.responseText;
+      let obj = JSON.parse(json);
+      let image = obj.cards;
+      var image_link = "";
+      for(let x = 0; x < image.length; x++){
+        if(image[x].hasOwnProperty('imageUrl')){
+          image_link = image[x].imageUrl;
+        }
       }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-  }
+      console.log(image_link);
+      // document.getElementById("demo").innerHTML += ("<img src=\"" + image_link + "\">");
+
+      document.getElementById("demo").innerHTML += ("<img src=\"" + image_link + "\" width=\"150px\" height=\"200px\" ondblclick = draftCard()>");
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function draftCard(){
+
 }
